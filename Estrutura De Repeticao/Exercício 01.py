9cb25c71606fc5047918817a629dd1f0a6e8ffe1
Jaosnake <jaosnake2004@yahoo.com.br>
2022-01-05 10:35:10 -0300

    Create Exercício 01.py

---
+++ Estrutura De Repeticao/Exercício 01.py
@@ -0,0 +1,9 @@
+print('=+' * 19)
+print('Exercício 01 - Estrutura de Repetição')
+print('=+' * 19)
+x = 0
+while 0 <= x <= 10:
+    x = float(input('Digite uma nota de 0 a 10: '))
+    if x < 0 or x > 10:
+        print ('Valor inválido!')
+        x = float(input('Digite uma nota de 0 a 10: '))
