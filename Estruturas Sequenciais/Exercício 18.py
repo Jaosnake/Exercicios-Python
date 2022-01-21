50b283f1412e75ab2a494a76f51631138167edb5
Jaosnake <jaosnake2004@yahoo.com.br>
2022-01-21 09:19:38 -0300

    Exercicios Resolvidos

--- Estruturas Sequenciais/Exercício 18.py
+++ Estruturas Sequenciais/Exercício 18.py
@@ -1,6 +1,6 @@
 arquivo = float(input('Tamanho do arquivo (MB): '))
 velocidade = float(input('Velocidade de Internet (MBps): '))
 seg = (arquivo / velocidade)
-min = seg/60
+minutos = seg / 60
 print(f'Tempo aproximado de download: {seg:.0f} segundos')
-print(f'Tempo aproximado de download: {min:.0f} minutos')
+print(f'Tempo aproximado de download: {minutos:.0f} minutos')
