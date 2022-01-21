cacde5f5b436905c1cf345c549a36516ff511a68
Jaosnake <jaosnake2004@yahoo.com.br>
2022-01-21 10:49:25 -0300

    Exercicios Resolvidos

--- Estrutura de Decisao/Exercicio 07.py
+++ Estrutura de Decisao/Exercicio 07.py
@@ -1,7 +1,7 @@
 n1 = input("Digite o primeiro número: ")
 n2 = input("Digite o segundo número: ")
 n3 = input("Digite o terceiro número: ")
-# Calculando menor valor digitado: (Por padrão, coloquei o A já sendo o de menor valor)
+# Calculando menor valor digitado: (Por padrão, coloquei o n1 já sendo o de menor valor)
 menor = n1
 if n2 <= n1 and n3:
     menor = n2
@@ -9,7 +9,7 @@ if n3 <= n1 and n3:
     menor = n3
 print(f'O menor número digitado foi {menor}')
 
-# Calculando maior valor digitado: (Por padrão, coloquei o A já sendo o de maior valor)
+# Calculando maior valor digitado: (Por padrão, coloquei o n1 já sendo o de maior valor)
 maior = n1
 if n2 > n1 and n3:
     maior = n2
