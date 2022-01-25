59ae59d4f6ba2ecc8319810841a8bab73f546cc0
Jaosnake <jaosnake2004@yahoo.com.br>
2022-01-25 08:03:50 -0300

    Exercicios Resolvidos

--- Estrutura de Decisao/Exercicio 17.py
+++ Estrutura de Decisao/Exercicio 17.py
@@ -1,7 +1,6 @@
 x = int(input("Digite o ano para verificar se ele é bissexto: "))
-bissexto = (x % 4)
+bissexto = x % 4
 if bissexto == 0:
     print("O ano digitado é bissexto")
 else:
     print("O ano digitado NÃO é bissexto")
-print("Fim do aplicativo")
