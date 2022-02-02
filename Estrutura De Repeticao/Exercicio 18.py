8f86e2839fb5cb0be1a3d3ffd878818117a2bef3
Jaosnake <jaosnake2004@yahoo.com.br>
2022-02-02 10:31:45 -0300

    Exercicios Resolvidos

--- Estrutura De Repeticao/Exercicio 18.py
+++ Estrutura De Repeticao/Exercicio 18.py
@@ -1,4 +1,17 @@
-for i in range (0,10):
-    print (i, end='.')
-    i+=1
-print (' = Fim')
+n = int(input('Digite o número de valores que quer calcular: '))
+soma = menor = maior = contador = 0
+
+for i in range (1, n+1):
+    v = int(input(f'Digite o {i}° valor: '))
+    soma +=v
+    contador+=1
+    if contador == 1:
+        maior = menor = v
+    else:
+        if v > maior:
+            maior = v
+        if v < menor:
+            menor = v
+print (f'A soma dos itens é igual a = {soma}')
+print (f'O menor dos valores é = {menor}')
+print (f'O maior dos valores é = {maior}')
--- Estrutura De Repeticao/Exercicio 19.py
+++ Estrutura De Repeticao/Exercicio 19.py
@@ -1,17 +1,20 @@
-n = int(input('Digite o número de valores que quer calcular: '))
 soma = menor = maior = contador = 0
-
+n = int(input('Digite o número de valores que quer calcular: '))
 for i in range (1, n+1):
     v = int(input(f'Digite o {i}° valor: '))
-    soma +=v
-    contador+=1
-    if contador == 1:
-        maior = menor = v
+    if v < 0 or v > 1000:
+        print ('Número inválido! Saindo do programa!')
+        break
     else:
-        if v > maior:
-            maior = v
-        if v < menor:
-            menor = v
+        soma +=v
+        contador+=1
+        if contador == 1:
+            maior = menor = v
+        else:
+            if v > maior:
+                maior = v
+            if v < menor:
+                menor = v
 print (f'A soma dos itens é igual a = {soma}')
 print (f'O menor dos valores é = {menor}')
 print (f'O maior dos valores é = {maior}')
